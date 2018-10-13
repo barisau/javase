e56a8e9a07dc65269ef2212cc64a0ca0d09af291
Sasha Borisov <35650271+barisau@users.noreply.github.com>
2018-10-13 12:14:19 +0300

    Add files via upload

---
+++ 1test/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //work1
+        String[] nickname = new String[10];
+        nickname[0] = "tourist";
+        nickname[1] = "Sasha228";
+        nickname[2] = "Agent2200";
+        nickname[3] = "papa";
+        nickname[4] = "HARDCORE";
+        nickname[5] = "PIXEL";
+        nickname[6] = "MAMA";
+        nickname[7] = "BORIS";
+        nickname[8] = "boris228";
+        nickname[9] = "88005553535";
+        System.out.println(nickname[0]);
+        System.out.println(nickname[1]);
+        System.out.println(nickname[2]);
+        System.out.println(nickname[3]);
+        System.out.println(nickname[4]);
+        System.out.println(nickname[5]);
+        System.out.println(nickname[6]);
+        System.out.println(nickname[7]);
+        System.out.println(nickname[8]);
+        System.out.println(nickname[9]);
+
+    }
+}
